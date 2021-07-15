fbd4669cee4d8d93cc3aedf0ab4d10f6af9eac77
wsigma21 <wallstlbl@gmail.com>
2021-07-15 21:06:12 +0900

    advance 16 検討

---
+++ advance/question16.sql
@@ -0,0 +1,21 @@
+-- グループCの各対戦毎にゴール数を表示してください。
+-- ゴール数がゼロの場合も表示してください。自国のゴール数は副問合せを用いて表示してください。
+-- ゴール数がゼロの場合も表示してください。問15の副問合せを用いたSQLにカラムを付けくわえる形で作成してください
+
+select * from pairings;
+select * from countries;
+select * from goals;
+
+select
+  date_format(pa.kickoff, '%Y-%d-%m') as disp_kickoff,
+  mc.name as 'my_country',
+  ec.name as 'enemy_country',
+  mc.ranking as 'my_ranking',
+  ec.ranking as 'enemy_ranking',
+  (select count(g.id) from goals g where pa.id = g.pairing_id) as goals
+  (select count(g.id from goals g2 where ))
+from pairings pa
+left outer join countries mc on pa.my_country_id = mc.id
+left outer join countries ec on pa.enemy_country_id = ec.id
+where mc.group_name = 'C' and ec.group_name = 'C'
+order by pa.kickoff, mc.ranking;
