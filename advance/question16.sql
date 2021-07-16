104f22521cc34da0358f5a44f2449d5cc2d3f5a2
wsigma21 <wallstlbl@gmail.com>
2021-07-16 20:51:14 +0900

    力試し16解答

--- advance/question16.sql
+++ advance/question16.sql
@@ -12,8 +12,28 @@ select
   ec.name as 'enemy_country',
   mc.ranking as 'my_ranking',
   ec.ranking as 'enemy_ranking',
-  (select count(g.id) from goals g where pa.id = g.pairing_id) as goals
-  (select count(g.id from goals g2 where ))
+  (select count(g.id) from goals g where pa.id = g.pairing_id) as 'my_goals',
+  (select count(g2.id) from goals g2 where g2.pairing_id = 
+    (select id from pairings pa2 where pa2.kickoff = pa.kickoff and pa2.my_country_id = pa.enemy_country_id)
+  ) as 'enemy_goals'
+from pairings pa
+left outer join countries mc on pa.my_country_id = mc.id
+left outer join countries ec on pa.enemy_country_id = ec.id
+where mc.group_name = 'C' and ec.group_name = 'C'
+order by pa.kickoff, mc.ranking;
+
+-- 解答
+select
+  date_format(pa.kickoff, '%Y-%d-%m') as disp_kickoff,
+  mc.name as 'my_country',
+  ec.name as 'enemy_country',
+  mc.ranking as 'my_ranking',
+  ec.ranking as 'enemy_ranking',
+  (select count(g.id) from goals g where pa.id = g.pairing_id) as 'my_goals',
+  (select count(g2.id) from goals g2
+    left outer join pairings pa2 on pa2.id = g2.pairing_id
+    where pa2.my_country_id = pa.enemy_country_id and pa2.enemy_country_id = pa.my_country_id
+  ) as 'enemy_goals'
 from pairings pa
 left outer join countries mc on pa.my_country_id = mc.id
 left outer join countries ec on pa.enemy_country_id = ec.id
